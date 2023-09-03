6ccc0f0fe4b33dc73efca7adbfcf9612663d5440
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2023-09-03 17:29:33 -0400

    Trying to implement test for dao.

--- spring-boot-example/src/main/java/com/cam/App.java
+++ spring-boot-example/src/main/java/com/cam/App.java
@@ -2,13 +2,13 @@ package com.cam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
+
 @SpringBootApplication
 
 public class App {
-     
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
- 
+
 }
--- spring-boot-example/src/main/java/com/cam/dao/UserDaoImpl.java
+++ spring-boot-example/src/main/java/com/cam/dao/UserDaoImpl.java
@@ -1,20 +1,27 @@
 package com.cam.dao;
 
 import java.sql.Date;
+import java.sql.Types;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.cam.entity.User;
 
 public class UserDaoImpl implements UserDao {
 	
+	private final Logger log = LogManager.getLogger(UserDaoImpl.class);
+	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	private List<User> query(final String sql, final String searchFor) {
+	private List<User> get(final String sql, final String searchFor) {
+		
 		List<User> userList = jdbcTemplate.query(sql, (rs, rowNum) -> {
         	String firstName = rs.getString("first_name");
         	String lastName = rs.getString("last_name");
@@ -30,41 +37,36 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public Optional<User> find(User arg) {
-		
 		String sql = "SELECT * FROM users WHERE users.user_name = ?";
         String username = "cameodud234";
         
-        List<User> user = query(sql, username);
+        List<User> user = get(sql, username);
         
         return Optional.ofNullable(user.get(0));
-        
 	}
 
 	@Override
 	public List<User> findAll() {
 		String sql = "SELECT * FROM users LIMIT 100";
-		List<User> userList = jdbcTemplate.query(sql, (rs, rowNum) -> {
-        	String firstName = rs.getString("first_name");
-        	String lastName = rs.getString("last_name");
-        	String userName = rs.getString("user_name");
-        	String email = rs.getString("email");
-        	String password = rs.getString("password");
-        	Date dateOfBirth = rs.getDate("date_of_birth");
-        	String description = rs.getString("description");
-        	return new User(firstName, lastName, userName, email, password, dateOfBirth, description);
-        });
-		
-		return userList;
+		List<User> users = get(sql, "");
+		return users;
 	}
 
 	@Override
 	public void add(User arg) {
-		
+		try {
+			
+		} catch (DataAccessException e) {
+			log.error(e.getMessage(), e.getCause(), e.getStackTrace());
+		}
+		String sql = "INSERT INTO Users (first_name, last_name, user_name, email, password, date_of_birth, description)\n";
+		Object[] args = {arg.getFirstName(), arg.getLastName(), arg.getUserName(), arg.getEmail(), arg.getPassword(), arg.getDate()};
+		int[] argTypes = {java.sql.Types.VARCHAR, java.sql.Types.VARCHAR, java.sql.Types.VARCHAR, java.sql.Types.VARCHAR, java.sql.Types.VARCHAR, java.sql.Types.DATE, Types.CLOB};
+		jdbcTemplate.update(sql, args, argTypes);
 	}
 
 	@Override
 	public void addAll(List<User> args) {
-		// TODO Auto-generated method stub
 		
 	}
 

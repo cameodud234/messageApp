26cadbd5da35201d53c83fe015afcf1b46ce89d5
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2023-09-05 20:23:23 -0400

    Got the junit integration tests to work.

--- spring-boot-example/src/test/java/com/cam/dao/TestUserDAOImpl.java
+++ spring-boot-example/src/test/java/com/cam/dao/TestUserDAOImpl.java
@@ -2,31 +2,47 @@ package com.cam.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import com.cam.entity.User;
 
+@SpringBootTest
+@ActiveProfiles("test")
 public class TestUserDAOImpl {
 	
 	private final Logger log = LogManager.getLogger(TestUserDAOImpl.class);
-
-    @Mock
-    private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private UserDAOImpl userDAOImpl;
 
     
     @Test
     public void testFind() {
-    	UserDAOImpl userDAOImpl = new UserDAOImpl();
-        ReflectionTestUtils.setField(userDAOImpl, "jdbcTemplate", jdbcTemplate);
-        Mockito.when(jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class))
-          .thenReturn(128260);
-
-        assertEquals(128260, userDAOImpl.getCountOfEmployees());
+    	User user = userDAOImpl.find("1");
+    	log.info("user:" + user);
+    	LocalDate date = LocalDate.of(1982, 4, 9);
+    	Date actualBirthday = Date.valueOf(date);
+    	assertEquals(user, new User("Bob", "Ross", "bross234", "bross2@gmail.com", "123", actualBirthday, ""));
+    }
+    
+    @Test 
+    public void testFinalAll() {
+    	List<User> users = userDAOImpl.findAll();
+    	User user1 = new User("Bob", "Ross", "bross234", "bross2@gmail.com", "123", Date.valueOf(LocalDate.of(1982, 4, 9)), "");
+    	User user2 = new User("Bob", "Ross", "bross234", "bross2@gmail.com", "123", Date.valueOf(LocalDate.of(1979, 1, 9)), "");
+    	List<User> expectedUsers = List.of(user1, user2);
+    	users.add(user1);
+    	users.add(user2);
+    	assertEquals(users, expectedUsers);
     }
 
 }

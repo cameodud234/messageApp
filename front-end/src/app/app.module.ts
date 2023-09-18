0685a8d88780ff3f9b54c18ec6fa9b93ae2e2ed3
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2023-09-17 20:14:15 -0400

    Update to angular and added bootstrap.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
